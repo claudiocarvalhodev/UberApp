a9660e498940908ff636f902e62640efc76dd147
claudiocarvalhodev <claudiocarvalhodev@gmail.com>
2020-02-28 18:51:04 -0800

    Add location input user interface and locations tableview with animation

---
+++ Uber/Scenes/Home/Components/Cell/LocationCell.swift
@@ -0,0 +1,55 @@
+//
+//  LocationCell.swift
+//  Uber
+//
+//  Created by claudiocarvalho on 28/02/20.
+//  Copyright © 2020 claudiocarvalho. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "123 Main Street"
+        return label
+    }()
+    
+    private let addressLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.text = "123 Main Street, Washington, DC"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helper Functions
+    
+    func configureUI() {
+        selectionStyle = .none
+        
+        let stack = UIStackView(arrangedSubviews: [titleLabel, addressLabel])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 4
+        
+        addSubview(stack)
+        stack.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+    }
+    
+}
