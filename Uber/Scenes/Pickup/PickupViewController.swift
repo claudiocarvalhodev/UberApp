2edb2ab0100ca0c0d7deee34151676c479e650fc
claudiocarvalhodev <claudiocarvalhodev@gmail.com>
2020-03-03 13:05:26 -0800

    Add fetch and accept trip and passenger side while trip is being accepted

---
+++ Uber/Scenes/Pickup/PickupViewController.swift
@@ -0,0 +1,127 @@
+//
+//  PickupViewController.swift
+//  Uber
+//
+//  Created by claudiocarvalho on 03/03/20.
+//  Copyright © 2020 claudiocarvalho. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+protocol PickupViewControllerDelegate: class {
+    func didAcceptTrip(_ trip: Trip)
+}
+
+class PickupViewController: UIViewController {
+    
+    // MARK: - Properties
+    weak var delegate: PickupViewControllerDelegate?
+    private let mapView = MKMapView()
+    let trip: Trip
+    
+    private let cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "baseline_clear_white_36pt_2x").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        return button
+    }()
+    
+    private let pickupLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Would you like to pickup this passenger?"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .white
+        return label
+    }()
+    
+    private let acceptTripButton: UIButton = {
+        let button = UIButton(type: .system)
+        //button.setImage(#imageLiteral(resourceName: <#T##String#>).withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleAcceptTrip), for: .touchUpInside)
+        button.backgroundColor = .white
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.setTitle("ACCEPT TRIP", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    init(trip: Trip) {
+        self.trip = trip
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleAcceptTrip() {
+        Service.shared.acceptTrip(trip: trip) { (error, ref) in
+            self.delegate?.didAcceptTrip(self.trip)
+        }
+    }
+    
+    @objc func handleDismissal() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - API
+    
+    
+    // MARK: - Helper Functions
+    
+    func configure() {
+        configureUI()
+        configureMapView()
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .uberGrayDark
+        
+        view.addSubview(cancelButton)
+        cancelButton.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                            left: view.leftAnchor,
+                            paddingLeft: 16)
+        cancelButton.setDimensions(height: 30, width: 30)
+        
+        view.addSubview(mapView)
+        mapView.setDimensions(height: 270, width: 270)
+        mapView.layer.cornerRadius = 270 / 2
+        mapView.centerX(inView: view)
+        mapView.centerY(inView: view, constant: -200)
+        
+        view.addSubview(pickupLabel)
+        pickupLabel.centerX(inView: view)
+        pickupLabel.anchor(top: mapView.bottomAnchor, paddingTop: 16)
+        
+        view.addSubview(acceptTripButton)
+        acceptTripButton.anchor(top: pickupLabel.bottomAnchor,
+                                left: view.leftAnchor,
+                                right: view.rightAnchor,
+                                paddingTop: 16, paddingLeft: 32, paddingRight: 32, height: 50)
+    }
+    
+    func configureMapView() {
+        let region = MKCoordinateRegion(center: trip.pickupCoordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: false)
+        
+        let placemark = MKPlacemark(coordinate: trip.pickupCoordinates)
+        let anno = MKPointAnnotation()
+        anno.coordinate = trip.pickupCoordinates
+        mapView.addAnnotation(anno)
+        self.mapView.selectAnnotation(anno, animated: true)
+    }
+}
